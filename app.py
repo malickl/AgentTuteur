4882df477300a2353edf0044b76fb8957c196f8c
Mahassadi Adou Malick Jean-marie <jean-marie@MacBookPro.lan>
2025-12-10 22:33:26 +0100

    ajout de l'interface streamlit

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
 import streamlit as st
+from APIgroq import Agent
+from APIgemini import Gemini
+
+GroqAgent = Agent()
+GeminiAgent = Gemini()
+
+
+FichierAudio = st.empty()
+
+st.title("Agent Tuteur")
+
+message = st.text_input("Entrez votre message")
+
+reponse = GroqAgent.ask(message)
+st.write(reponse)
+
+GeminiAgent.audio(reponse)
+
+
+with open("résumé.wav", "rb") as f:
+    audio_bytes = f.read()
+
+FichierAudio.audio(audio_bytes, format="audio/wav")
+
+
+
